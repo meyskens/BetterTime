81d664e6dbf286e17ce1ebc91b78918924bf9a6d
Maartje Eyskens <maartje@eyskens.me>
2022-08-23 11:55:33 +0200

    use teh echo rewriter

--- cmd/bettertime/serve.go
+++ cmd/bettertime/serve.go
@@ -58,17 +58,12 @@ func (s *serveCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 	e.Use(middleware.CORS())
 
 	// rewrite paths that frontend uses
-	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			if c.Request().URL.Path == "/rooms" {
-				c.Request().URL.Path = "/"
-			}
-			return h(c)
-		}
+	e.Pre(middleware.Rewrite(map[string]string{
+		"/rooms": "/",
+	}))
 
 	// serve Swagger as static content
 	e.Static("/", "static")
-
 
 	// register the HTTP handlers
 	v1.NewHTTPHandler(s.baseURL).Register(e)
