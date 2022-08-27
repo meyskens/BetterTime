e34cd4d4d4aee3cab8e45cfc6b236e04c879e5bf
Maartje Eyskens <maartje@eyskens.me>
2022-08-27 17:42:55 +0200

    add egg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+console.error("Elke gelijkenis met SAT berust op louter toeval.");
+console.error("Hallo daar! Leuk dat je eventjes komt kijken... maar alles is gewoon open source hoor!");
+console.error("Anyhow als je graag ook je naam bij collega's hebt, de prijs is 1 big c koffie of gelijkgestelde vriendelijkheid");
+
 ReactDOM.render(
   <Suspense fallback="...">
     <React.StrictMode>
