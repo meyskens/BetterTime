34329274cb0463a989a836e9b77d1ab0e9b920c7
Maartje Eyskens <maartje@eyskens.me>
2022-09-13 11:00:50 +0200

    Add LTI and TTI

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
-export const CLASS_SEARCH = "GTI TI 1ITF, GTI TI 2, GTI EI 2, GTI TI 3, GTI EI 3, GTI ACS, GTI GNB, GTI GPR";
+export const CLASS_SEARCH = "GTI TI 1ITF, GTI TI 2, GTI EI 2, GTI TI 3, GTI EI 3, GTI ACS, GTI GNB, GTI GPR, LTI GPR, LTI GNB, TTI GPR";
